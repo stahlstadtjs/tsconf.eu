e2d4a9b2b3511fc4988c356f182502c7312e12fa
Dominik Angerer <dominikangerer1@gmail.com>
2019-12-24 17:03:06 +0100

    pure text styles

--- components/PureText.vue
+++ components/PureText.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="pure-text" v-editable="blok">
-    <div v-html="$md.render(blok.text)"></div>
+  <div class="pure-text py-32" v-editable="blok">
+    <div class="container mx-auto px-8">
+      <div class="max-w-5xl markdown" v-html="$md.render(blok.text)"></div>
+    </div>
   </div>
 </template>
 
@@ -9,3 +11,21 @@ export default {
   props: ['blok']
 }
 </script>
+
+<style>
+.markdown h1 {
+  @apply text-red uppercase font-bold text-6xl leading-tight mb-5;
+}
+
+.markdown h2 {
+  @apply text-blue font-semibold text-3xl leading-tight mb-3;
+}
+
+.markdown h3 {
+  @apply text-black font-semibold text-2xl leading-tight mb-3;
+}
+
+.markdown p {
+  @apply text-xl mb-12 whitespace-pre-line;
+}
+</style>
