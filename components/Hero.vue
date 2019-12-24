d6a5d9686b308008f76ee15958faa6ed074fb720
Dominik Angerer <dominikangerer1@gmail.com>
2019-12-24 17:17:36 +0100

    adds basic teaser styling

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,11 +1,17 @@
 <template>
-  <div class="hero" v-editable="blok">
-    <h2>Hero</h2>
-    {{blok.headline}}<br>
-    {{blok.subheadline}}<br>
-    {{blok.text}}<br>
-    {{blok.image}}<br>
-    {{blok.alt}}
+  <div class="hero bg-light-gray" v-editable="blok">
+    <div class="container px-8 py-24">
+      <div class="flex">
+        <div class="w-2/5">
+          <h1 class="text-red uppercase font-semibold text-6xl">{{blok.headline}}</h1>
+          <h2 class="text-red font-semibold leading-tight text-3xl mb-4">{{blok.subheadline}}</h2>
+          <p class="text-blue">{{blok.text}}</p>
+        </div>
+        <div class="w-3/5 px-12">
+          <img class="float-right" :src="$options.filters.transformImage(blok.image, '500x0')" :alt="blok.alt">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
