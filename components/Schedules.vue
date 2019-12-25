b1a4a24cad2f6e1a436647f45a907ad5f3923970
Dominik Angerer <dominikangerer1@gmail.com>
2019-12-25 15:58:09 +0100

    adds styling for schedules

--- components/Schedules.vue
+++ components/Schedules.vue
@@ -3,15 +3,17 @@
     <div class="container px-8 pt-12 pb-24">
       <div class="flex">
         
-        <div class="w-1/2 schedule" v-for="schedule in blok.schedules" :key="schedule._uid" v-editable="schedule">
+        <div class="w-1/2 schedule" v-for="(schedule, index) in blok.schedules" :key="schedule._uid" v-editable="schedule">
           <h3>
-            <div class="text-blue float-right mt-2">{{schedule.notice}}</div>
-            <span class="text-blue font-bold text-3xl uppercase block">{{schedule.headline}}</span>
+            <span class="text-blue font-bold text-3xl uppercase block">
+              {{schedule.headline}}
+              <span class="text-blue font-normal text-base">{{schedule.notice}}</span>
+            </span>
             <span class="text-blue font-bold text-6xl block" v-html="schedule.date"></span>
           </h3>
           
-          <ul>
-            <li class="flex py-6 pr-12" v-for="scheduleItem in schedule.schedule_items" :key="scheduleItem._uid" v-editable="scheduleItem">
+          <ul class="border-blue mr-10 my-8" :class="{ 'border-r': index === 0 }">
+            <li class="flex py-6 border-b mr-10 border-blue" :class="{ 'border-t': index === 0 }" v-for="(scheduleItem, index) in schedule.schedule_items" :key="scheduleItem._uid" v-editable="scheduleItem">
               <div class="w-1/4">
                 <span class="text-blue font-bold text-2xl">{{scheduleItem.time}}</span>
               </div>
@@ -19,16 +21,16 @@
                 <h4 class="text-red font-semibold leading-tight text-3xl mb-4">{{scheduleItem.headline}}</h4>
                 <div class="flex">
                   <img class="rounded-full" v-if="scheduleItem.image" :src="$options.filters.transformImage(scheduleItem.image, '80x80')" alt="">
-                  <p v-if="scheduleItem.text" class="text-blue">{{scheduleItem.text}}</p>
+                  <div v-if="scheduleItem.text" class="text-blue" v-html="$md.render(scheduleItem.text)"></div>
                 </div>
               </div>
               <div class="w-3/4" v-if="scheduleItem.speaker">
                 <h4 class="text-red font-semibold leading-tight text-3xl mb-4">{{speakers[scheduleItem.speaker].name}}</h4>
                 <div class="flex">
                   <img class="rounded-full" :src="$options.filters.transformImage(speakers[scheduleItem.speaker].content.image, '80x80')" :alt="speakers[scheduleItem.speaker].name">
-                  <p v-if="scheduleItem.text" class="text-blue pl-4">{{scheduleItem.text}}</p>
+                  <div v-if="scheduleItem.text" class="text-blue pl-4" v-html="$md.render(scheduleItem.text)"></div>
                 </div>
-              </div> 
+              </div>
             </li>
           </ul>
 
@@ -56,3 +58,12 @@ export default {
   }
 }
 </script>
+
+<style>
+.schedule a {
+  @apply text-red border-b-0;
+}
+.schedule a:hover, .schedule a:focus {
+  @apply border-b-2;
+}
+</style>
