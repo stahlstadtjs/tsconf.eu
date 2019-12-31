0bba28e7b8e43d9552a81312a2987c3e76d79bdb
Dominik Angerer <dominikangerer1@gmail.com>
2019-12-31 18:23:54 +0100

    removes noindex

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
-      { name: 'robots', content: 'noindex' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
