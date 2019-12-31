b2e32831b4aff17f3c5a65a1258fdc36ae9355c2
Dominik Angerer <dominikangerer1@gmail.com>
2020-01-01 00:21:03 +0100

    adds favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,26 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { name: "msapplication-TileColor", content: "#ffffff"},
+      { name: "msapplication-TileImage", content: "/images/ms-icon-144x144.png" },
+      { name: "theme-color", content: "#ffffff" }
     ],
     link: [
+      { rel: "manifest", href: "/manifest.json" },
+      { rel: "apple-touch-icon", sizes: "57x57", href: "/images/apple-icon-57x57.png" },
+      { rel: "apple-touch-icon", sizes: "60x60", href: "/images/apple-icon-60x60.png" },
+      { rel: "apple-touch-icon", sizes: "72x72", href: "/images/apple-icon-72x72.png" },
+      { rel: "apple-touch-icon", sizes: "76x76", href: "/images/apple-icon-76x76.png" },
+      { rel: "apple-touch-icon", sizes: "114x11,4", href: "/images/apple-icon-114x114.png" },
+      { rel: "apple-touch-icon", sizes: "120x12,0", href: "/images/apple-icon-120x120.png" },
+      { rel: "apple-touch-icon", sizes: "144x14,4", href: "/images/apple-icon-144x144.png" },
+      { rel: "apple-touch-icon", sizes: "152x15,2", href: "/images/apple-icon-152x152.png" },
+      { rel: "apple-touch-icon", sizes: "180x18,0", href: "/images/apple-icon-180x180.png" },
+      { rel: "icon", type: "image/png", sizes: "192x192", href: "/images/android-icon-192x192.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", href: "/images/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "96x96", href: "/images/favicon-96x96.png" },
+      { rel: "icon", type: "image/png", sizes: "16x16", href: "/images/favicon-16x16.png" },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel:'stylesheet', type: 'text/css', href: 'https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap' }
     ]
