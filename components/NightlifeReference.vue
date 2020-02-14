fa0376b32c98d86320cd98f74332550b929bfcfb
Dominik Angerer <dominikangerer1@gmail.com>
2020-02-14 10:10:01 +0000

    adds social

--- components/NightlifeReference.vue
+++ components/NightlifeReference.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="nightlife-reference" v-editable="blok">
-    <h2>NightlifeReference</h2>
-    {{blok.headline}}<br>
-    {{blok.subheadline}}<br>
-    {{blok.cta_headline}}<br>
-    {{blok.cta_link}}<br>
-    {{blok.cta_text}}<br>
-    {{blok.cta_title}}
-
-    <ul>
-      <li v-for="nightlife in nightlifeEntries" :key="nightlife.uuid">
-        {{nightlife}}
-      </li>
-    </ul>
+  <div class="nightlife-references bg-light-gray" v-editable="blok">
+    <div class="container px-8 py-24">
+      <div class="w-full lg:w-2/5">
+        <h2 class="headline">{{blok.headline}}</h2>
+        <p class="text-red font-semibold leading-tight text-3xl mb-4">{{blok.subheadline}}</p>
+        <p class="text-blue">{{blok.text}}</p>
+      </div>
+      <ul class="flex flex-wrap mt-12">
+        <li class="w-1/2 lg:w-1/3 mb-12 pr-12 text-center" v-for="nightlife in nightlifeEntries" :key="nightlife.uuid">
+          <a :href="nightlife.gmaps_link" :title="'Open Google Maps to ' + nightlife.name">
+            <div class="ib">
+              <img loading="lazy" :src="nightlife.content.image || 'https://source.unsplash.com/collection/190727/1600x900'" :alt="nightlife.name">
+            </div>
+            <h3 class="relative text-red font-semibold uppercase sm:text-lg lg:text-2xl -mt-24 p-4 leading-none bg-white inline-block">{{nightlife.name}}</h3>
+            <p class="text-blue p-6">{{nightlife.content.description}}</p>
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
--- components/SightseeingReference.vue
+++ components/SightseeingReference.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="sightseeing-reference" v-editable="blok">
-    <h2>SightseeingReference</h2>
-    {{blok.headline}}<br>
-    {{blok.subheadline}}
-
-    <ul>
-      <li v-for="sightseeing in sightseeingEntries" :key="sightseeing.uuid">
-        {{sightseeing}}
-      </li>
-    </ul>
+  <div class="sightseeing-references bg-light-gray" v-editable="blok">
+    <div class="container px-8 py-24">
+      <div class="w-full lg:w-2/5">
+        <h2 class="headline">{{blok.headline}}</h2>
+        <p class="text-red font-semibold leading-tight text-3xl mb-4">{{blok.subheadline}}</p>
+        <p class="text-blue">{{blok.text}}</p>
+      </div>
+      <ul class="flex flex-wrap mt-12">
+        <li class="w-1/2 lg:w-1/3 mb-12 pr-12 text-center" v-for="sightseeing in sightseeingEntries" :key="sightseeing.uuid">
+          <a :href="sightseeing.gmaps_link" :title="'Open Google Maps to ' + sightseeing.name">
+            <div class="ib">
+              <img loading="lazy" :src="sightseeing.content.image || 'https://source.unsplash.com/collection/190727/1600x900'" :alt="sightseeing.name">
+            </div>
+            <h3 class="relative text-red font-semibold uppercase sm:text-lg lg:text-2xl -mt-24 p-4 leading-none bg-white inline-block">{{sightseeing.name}}</h3>
+          </a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
